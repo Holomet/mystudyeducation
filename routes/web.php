c2d479a853e2a0c1ed65e6f895ba8819b9b71763
holomet <sreetkt@gmail.com>
2021-07-27 10:28:19 +0530

    Home page

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
 	return view('home.index');
 });
-Route::any('/admin', function () {
-    return redirect('login');
-});
 
 Auth::routes();
 
